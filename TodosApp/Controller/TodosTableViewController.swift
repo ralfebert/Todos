e4c057ca7e8e5ac34d28277d36b51282547660c4
Ralf Ebert <info@ralfebert.de>
2019-09-09 18:17:19 +0200

    app icon hinzugefügt

--- TodosApp/Controller/TodosTableViewController.swift
+++ TodosApp/Controller/TodosTableViewController.swift
@@ -38,7 +38,6 @@ class TodosTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return self.todos.count
     }
 
